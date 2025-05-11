fd5e00e80d2b8584b05e385b44025710eceeb73a
Dang <tonminhdang9@gmail.com>
2025-05-11 19:24:49 +0700

    thêm API gọi sản phẩm

---
+++ src/api/products/get_product.php
@@ -0,0 +1,17 @@
+<?php
+    require_once 'product.php';
+
+    if (isset($_POST['style'])){
+        $style = $_POST['style'];
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST"){
+            echo get_product($style);
+        }
+        else{
+            echo "request method not access";
+        }
+    }
+    else{
+        echo "Need POST['style'] to get data";
+    }
+?>
