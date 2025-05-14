ace8750c73f5408383020b4c7d8099ed34c3b8f5
Viet Quan Phan <vquan29905l@gmail..com>
2025-05-15 02:05:53 +0700

    load lại

--- src/connect.php
+++ src/connect.php
@@ -1,13 +1,13 @@
 <?php
-    $host = 'localhost';
-    $db = 'final_se_db';
-    $user = 'root';
-    $pass = '';
+    // $host = 'localhost';
+    // $db = 'final_se_db';
+    // $user = 'root';
+    // $pass = '';
 
-    // $host = 'db';     
-    // $db   = 'final_se';       
-    // $user = 'user';           
-    // $pass = 'password';
+    $host = 'db';     
+    $db   = 'final_se';       
+    $user = 'user';           
+    $pass = 'password';
 
     $conn = new mysqli($host, $user, $pass, $db);
 
